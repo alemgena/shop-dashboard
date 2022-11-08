53b3afccec0ee7d3040ab4b4816b3622c9a26f56
alemgena <alemgenateferi1@gmil.com>
2022-11-08 01:00:49 -0800

    change url

--- src/utiles/config.js
+++ src/utiles/config.js
@@ -1,3 +1,3 @@
-export const url = 'http://localhost:8080'
+export const url = 'https://142.93.222.117'
 //export const url = 'http://192.168.1.11:4590/api'
 //export const url = 'http://206.81.27.23:4590/api'
